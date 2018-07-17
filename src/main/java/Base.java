48288a993ad038b02ba7c12a6588c7cc2fe42980
savodka <rammstein11>
2018-07-17 19:11:11 +0300

    1 commit

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Hello world, wtf");
+        //float var1, var2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        float var1 = scanner.nextInt();
+        System.out.println("Enter the second number ");
+        float var2 = scanner.nextInt();
+        float sum = var1 + var2;
+        /*System.out.println("Enter the first number " + var1);
+        System.out.println("Enter the second number " + var2);*/
+        System.out.printf("Summ is " + sum);
+        scanner.close();
     }
 }
