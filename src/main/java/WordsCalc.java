478fe93242cb49868dfc4c256b4e774401ac355a
savodka <rammstein11>
2018-07-24 18:11:53 +0300

    new lesson, added words search, final ver

--- src/main/java/WordsCalc.java
+++ src/main/java/WordsCalc.java
@@ -9,42 +9,23 @@ public class WordsCalc {
         System.out.println("Please enter the array size");
         int n = scanner.nextInt();
         //int [] a = new int [n];
-        System.out.println("Enter " + n +" words into your array");
+        System.out.println("Enter " + n + " strings into your array");
 
         String[] strings = new String[n];
 
-        for (int i=0; i<n; i++) {
-            System.out.print("Enter string " + (i+1) + ": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter string " + (i + 1) + ": ");
             strings[i] = scanner.next();
         }
-
-        /*System.out.println("The biggest word in your array: ");
-
-        System.out.println(Arrays.toString(strings));
-        for (int i = 0; i < strings. length; i++){
-            System.out.println (strings[i]);
-        }
-        */
         int maxLengthString = 0;
-        for(int i=0;i<strings.length;i++){
-            if(strings[i].length()>maxLengthString){
+        String longestWord = null;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() > maxLengthString) {
                 maxLengthString = strings[i].length();
+                longestWord = strings[i];
             }
         }
-        for(int i=0;i<strings.length;i++){
-            if(strings[i].length()==maxLengthString){
-                System.out.println("The biggest word in your array: " + strings[i]);
-            }
-        }
-
-
-            /*int maximum = string[0];
-
-            for (int i = 0; i < string.length; i++)
-                if (maximum < string[i]) maximum = string[i];
-
-            return maximum;*/
-
+        System.out.println("The biggest word in your array: " + longestWord);
         scanner.close();
     }
 }
