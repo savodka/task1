b4bbb8af78fc9e5290ad52db24fe181cd880dd0d
savodka <rammstein11>
2018-07-24 18:16:20 +0300

    new lesson, added words search, final ver 1.1

--- src/main/java/WordsCalc.java
+++ src/main/java/WordsCalc.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
 
-
-import java.util.*;
-
 public class WordsCalc {
     public static void words() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the array size");
         int n = scanner.nextInt();
-        //int [] a = new int [n];
         System.out.println("Enter " + n + " strings into your array");
-
         String[] strings = new String[n];
-
         for (int i = 0; i < n; i++) {
             System.out.print("Enter string " + (i + 1) + ": ");
             strings[i] = scanner.next();
