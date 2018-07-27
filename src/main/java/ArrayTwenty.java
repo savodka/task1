a98ab98b733f5ff894f5af01d8254d4841509674
savodka <rammstein11>
2018-07-27 19:09:47 +0300

    new lesson, added words search, added arrays with numbers

--- src/main/java/ArrayTwenty.java
+++ src/main/java/ArrayTwenty.java
@@ -3,23 +3,23 @@ import java.util.Random;
 public class ArrayTwenty {
     public static void arrtw() {
         Random random = new Random();
-        int intArray[]=new int[20];
+        int intArray[] = new int[20];
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i]=random.nextInt(21)-10;
+            intArray[i] = random.nextInt(21) - 10;
             System.out.println(intArray[i]);
         }
-        double max = Double.MIN_VALUE;
-        for (int i = 0; i < intArray.length; i++){
-            max = Math.max(max, intArray[i]);
-
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < intArray.length; i++) {
+            if (max < intArray[i] && intArray[i] < 0)
+                max = intArray[i];
         }
-        System.out.println("Максимальный элемент в массиве: " + max);
-        double min = Double.MIN_VALUE;
-        for (int i = 0; i < intArray.length; i++){
-            min = Math.min(min, intArray[i]);
+        System.out.println("Максимальный отрицательный элемент в массиве: " + max);
 
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < intArray.length; i++) {
+            if (min > intArray[i] && intArray[i] > 0)
+                min = intArray[i];
         }
-        System.out.println("Минимальный элемент в массиве: " + min);
+        System.out.println("Минимальный положительный элемент в массиве: " + min);
     }
-
 }
