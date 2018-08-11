83520d1d139b16a021ecde99ce960fb234ade4f4
savodka <rammstein11>
2018-08-11 10:51:28 +0300

    added 6 unit+ input.txt file

--- src/main/java/WordsCollection.java
+++ src/main/java/WordsCollection.java
@@ -8,18 +8,35 @@
 Вывести на консоль это слово и сколько раз оно встречается в файле
  */
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class WordsCollection {
+
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("input.txt"), Charset.defaultCharset());
-        System.out.println(lines);
 
+        System.out.println(lines);
+        System.out.println(lines.get(1));
+        lines.add("америка");
+
+        Collections.sort(lines);
+        System.out.println(lines);
+        System.out.println(lines.remove(0));
+        System.out.println(lines);
+        System.out.println(lines.get(2));
+        HashMap<String, Integer> hm = new HashMap<>();
+        Integer am;
+        for (String i : lines) {
+            am = hm.get(i);
+            if (am == null) hm.put(i, 1);
+            else hm.put(i, am + 1);
+        }
+        System.out.println(hm);
+        System.out.println(hm.values());
 
     }
 }
