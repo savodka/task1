3c59c70f922571a4b3e4fd7fcad4bbfb06deb880
savodka <rammstein11>
2018-08-30 15:27:10 +0300

    collection sorting + max values

--- src/main/java/WordsCollection.java
+++ src/main/java/WordsCollection.java
@@ -30,10 +30,39 @@ public class WordsCollection {
             else hm.put(i, am + 1);
         }
         System.out.println(hm);
-        //final Set<Map.Entry<String, Integer>> entries = hm.entrySet();
+        int maxKey = 0;
+        String maxRepeat = null;
+        for (String word : hm.keySet()) {
+            if (hm.get(word) > maxKey) {
+                maxKey = hm.get(word);
+                maxRepeat = word;
+            }
+
+        }
+        System.out.println("Максимально " + maxRepeat + " - " + Collections.max(hm.values()) + " раз");
     }
 }
 
+/*
+Если у тебя уже есть HashMap с подсчитанными повторениями,
+то проще всего пройти по нему циклом и найти максимальное значение "вручную".
+
+int maxRepeat=0;
+for (String word:hm.keySet()){
+    //тут должен быть твой код)
+}
+
+Это вроде как самый наглядный способ.
+Самый короткий - через вспомогательные методы класса Collections и список значений HashMap-а:
+
+maxRepeat = Collections.max(hm.values());
+
+
+А когда получил максимальную длину, то пройти ещё раз,
+ и вывести слова у которых число повторов максимально.
+ Бонусом получишь как раз то, что если есть несколько таких слов,
+ то выведутся все они, а не только одно.
+*/
 
 /*
 import java.io.BufferedReader;
